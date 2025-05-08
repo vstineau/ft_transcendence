f0192a4fbff4e4364bc8a2f007d95d6521a2ddf0
vstineau <vstineau@student.42.fr>
2025-05-08 22:43:08 +0200

    fix host

--- srcs/backend/index.ts
+++ srcs/backend/index.ts
@@ -27,7 +27,7 @@ server.get('/ping', async (_request, _reply) => {
   return 'pong\n'
 })
 
-server.listen({ port: 8080 }, (err, address) => {
+server.listen({ port: 8080, host: '0.0.0.0' }, (err, address) => {
   if (err) {
     console.error(err)
     process.exit(1)
