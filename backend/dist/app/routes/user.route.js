024db4640173abd1c10b17ccd1c23034fca93b41
Vincent Stineau <vstineau@f2r2s7.clusters.42paris.fr>
2025-08-14 10:06:51 +0200

    no dist in backend

--- backend/dist/app/routes/user.route.js
+++
@@ -1,5 +0,0 @@
-import { userController } from '../controller/user.controller.js';
-export default async function userRoute(fastify) {
-    fastify.register(userController);
-}
-//# sourceMappingURL=user.route.js.map
