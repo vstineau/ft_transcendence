672ba009629027de2bb81c3ecc7a9f69f5548363
William Nocchi <wnocchi@f1br5s1.clusters.42paris.fr>
2025-09-08 16:05:47 +0200

    tournament finished

--- frontend/src/types/pongTypes.ts
+++ frontend/src/types/pongTypes.ts
@@ -47,18 +47,10 @@ interface Round {
 
 export class Tournament {
 	players: PlayerTournament[] = [];
-	qualified: PlayerTournament[] = []; // surement de la merde
 	leaderBoard: PlayerTournament[] = [];
-	rounds: Round; // pk pas?
-
-	// canvas: HTMLCanvasElement | null = null;
-	// ctx: CanvasRenderingContext2D | null = null;
-	// form: HTMLFormElement | null = null;
-
+	rounds: Round;
 	win_width: number = window.innerWidth;
 	win_height: number = window.innerHeight;
-	// gameWidth: number = this.win_width * 0.6;
-	// gameHeight: number = this.win_height * 0.6;
 
 	constructor(names: string[]) {
 		this.rounds = { games: [], max: names.length === 8 ? 2 : 1, nb: 0 };
@@ -92,53 +84,30 @@ export class Tournament {
 			const j = Math.floor(Math.random() * (i + 1));
 			[this.players[i], this.players[j]] = [this.players[j], this.players[i]];
 		}
-		this.qualified = this.players; // surement de la merde
 	}
 	private initRounds() {
 		let nbgames = this.players.length === 8 ? 4 : 2;
 		for (let i = 0; i <= this.rounds.max; i++) {
 			let newRound: Game[] = [];
 			for (let j = 0; j < nbgames; j++) {
-				// console.log(`game ${j + 1} created`);
 				const newGame = this.gameInit(this.initPlayer('...'), this.initPlayer('...'));
 				newRound.push(newGame);
 			}
-			// console.log(`round ${i + 1} pushed`);
 			this.rounds.games.push(newRound);
-			// console.log(`nb games = ${nbgames}`);
 			nbgames /= 2;
 		}
 	}
 	private fillMatchs(round: Game[], p: PlayerTournament[]) {
 		if (!this) return;
-		// Remplir chaque game avec deux joueurs successifs
 		for (let i = 0; i < round.length; i++) {
 			const idx = i * 2;
-			// if (p.length < this.players.length) {
-			// 	p[idx].totalScore += 2;
-			// 	p[idx + 1].totalScore += 2;
-			// }
-			// p[idx].totalScore += p[idx].score;
-			// p[idx + 1].totalScore += p[idx + 1].score;
 			round[i] = this.gameInit(p[idx], p[idx + 1]);
-			// round[i].p1 = this.initPlayer(p[idx].nickName);
-			// round[i].p2 = this.initPlayer(p[idx + 1].nickName);
-			// round[i].p2.x = window.innerWidth * 0.98;
-			// round[i].p1.x = 20;
 			round[i].p1.score = 0;
 			round[i].p2.score = 0;
 		}
-		// const sorted = this.players.sort((a, b) => b.totalScore - a.totalScore);
-		// for (const sort of sorted) {
-		// 	console.log(sort.nickName + ' = ' + sort.totalScore);
-		// }
 	}
 	fillRound() {
 		if (!this) return;
-		// for(const player of this.players){
-		// 	player.totalScore += player.score
-		// 	player.score = 0;
-		// }
 		let qualified = this.players.filter(player => !player.eliminated);
 		this.fillMatchs(this.rounds.games[this.rounds.nb], qualified);
 	}
